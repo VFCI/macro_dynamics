8626dec875f2d33f96c1e92a1382440a3d0f3ac3
Fernando Duarte <duarte@alum.mit.edu>
2024-07-19 18:25:33 -0400

    Improve figures

--- create_figures.R
+++ create_figures.R
@@ -8,33 +8,30 @@ library(paletteer)
 output_dir <- "output/baseline/figures/"
 colors <- c("#4E8542","#2166AC","#8E0152","darkorange")
 vfci_color <- paletteer_d("ggthemes::excel_Ion")[1] #colors[1]
-
-
-## Figure 1. Standardized FCI figure ---------------------------------------
-# pick dates, variables
-
+fig_width <- 5 #in inches
+fig_height <- 5 #in inches
 base::load(file = here::here("variables.RData"))
+
+## Standardized FCI ---------------------------------------
+# pick dates, variables
 date_begin <- "1970 Q1"
 date_end <- "2022 Q3"
 
-variables_fig1 <- variables %>%
+variables_fig <- variables %>%
   dplyr::select(yr,qtr, nfci,gsfci,vixcls) %>%
   tsibble::as_tsibble() %>% 
   tsibble::filter_index(date_begin ~ date_end) 
 
-#variables_fig1$vfci_new <- 0
+tibble::add_column(variables_fig, vfci_new = (variables_fig %>%  select(yr,qtr, nfci,gsfci,vixcls)) )
 
-tibble::add_column(variables_fig1, vfci_new = (variables_fig1 %>%  select(yr,qtr, nfci,gsfci,vixcls)) )
-
-
-p <- ggplot(variables_fig1, aes(qtr)) + 
+p <- ggplot(variables_fig, aes(qtr)) + 
   geom_line(aes(y=scale(nfci),colour="NFCI"),na.rm=FALSE) +
   geom_line(aes(y=scale(gsfci),colour="GSFCI"),na.rm=FALSE) +
   geom_line(aes(y=scale(vixcls),colour="VIX"),na.rm=FALSE) +
   tsibble::scale_x_yearquarter(
     name = "",
     date_labels="%Y-q%q",
-    breaks = variables_fig1$qtr[seq(1, length(variables_fig1$qtr), 40)]
+    breaks = variables_fig$qtr[seq(1, length(variables_fig$qtr), 40)]
   ) +
   theme_classic() +
   theme(
@@ -45,32 +42,30 @@ p <- ggplot(variables_fig1, aes(qtr)) +
     axis.title.y=element_blank(),
     panel.border = element_rect(colour = "black", fill=NA, size=0.5)
   ) + 
-  scale_color_manual(values=colors[1:3])
+  scale_color_manual(values=colors[1:3]) +
   ylim(-2, 6)
 p %>% print
 
 fname <- here::here(paste0(output_dir,"FCI_std.png"))
-ggsave(fname)
+ggsave(fname, width = fig_width, height = fig_height)
 
 
-##  Figure 2. VFCI ----------------------------------------------------------
-
-base::load(file = here::here("variables.RData"))
+##  Figure. VFCI ----------------------------------------------------------
 date_begin <- "1960 Q1"
 date_end <- "2022 Q3"
 
-variables_fig2 <- variables %>%
+variables_fig <- variables %>%
   dplyr::select(yr,qtr, vfci) %>%
   tsibble::as_tsibble() %>% 
   tsibble::filter_index(date_begin ~ date_end)
 
-p <- ggplot(as.data.frame(variables_fig2), aes(qtr)) +
+p <- ggplot(as.data.frame(variables_fig), aes(qtr)) +
   geom_line(aes(y=vfci,colour="VFCI"),na.rm=FALSE)  +
   scale_fill_hue(l=40) +
   tsibble::scale_x_yearquarter(
     name = "",
     date_labels="%Y-q%q",
-    breaks = variables_fig2$qtr[seq(1, length(variables_fig2$qtr), 40)]
+    breaks = variables_fig$qtr[seq(1, length(variables_fig$qtr), 40)]
   ) +
   ylim(-1, 1.5) +
   theme_classic() +
@@ -83,19 +78,18 @@ p <- ggplot(as.data.frame(variables_fig2), aes(qtr)) +
 p %>% print
 
 fname <- here::here(paste0(output_dir,"just_vfci.png"))
-ggsave(fname)
+ggsave(fname, width = fig_width, height = fig_height)
 
-## Figure 3. Conditional mean and variance --------------------------------
-base::load(file = here::here("variables.RData"))
+## Conditional mean and variance --------------------------------
 date_begin <- "1960 Q1"
 date_end <- "2022 Q3"
 
-variables_fig3 <- variables %>%
-  dplyr::select(yr,qtr, vfci,mu) %>%
+variables_fig <- variables %>%
+  dplyr::select(yr, qtr, vfci, mu) %>%
   tsibble::as_tsibble() %>% 
   tsibble::filter_index(date_begin ~ date_end)
 
-p <- ggplot(as.data.frame(variables_fig3), aes(x=vfci,y=mu)) +
+p <- ggplot(as.data.frame(variables_fig), aes(x=vfci,y=mu)) +
   geom_point(aes(color="Fitted values"),shape="diamond",size=3) +
   geom_smooth(aes(color="OLS line"),method=lm, se=FALSE,linewidth=0.5) +
   ylab("Conditional Mean of GDP Growth") +
@@ -116,33 +110,34 @@ p <- ggplot(as.data.frame(variables_fig3), aes(x=vfci,y=mu)) +
     legend.title=element_blank(),
     legend.position = c(0.85,0.9),
     legend.direction="vertical"
-  ) 
+  ) +
+  ylim(-1.5, 1.5)+
+  xlim(-0.5, 1.25)
 p %>% print
 
 fname <- here::here(paste0(output_dir,"musigma_gdp.png"))
-ggsave(fname)
+ggsave(fname, width = fig_width, height = fig_height)
 
-## Figure 9. GDP and PCE VFCI ---------------------------------------------
-base::load(file = here::here("variables.RData"))
+## GDP and PCE VFCI ---------------------------------------------
 date_begin <- "1960 Q1"
 date_end <- "2022 Q3"
 
 vfci_cons <- dplyr::select(results$fgr1.pcecc96$ts, c("qtr","vfci")) %>% dplyr::rename(vfci_pce = vfci)
 
-variables_fig9 <- variables %>%
+variables_fig <- variables %>%
   dplyr::inner_join(vfci_cons,by="qtr") %>%
-  dplyr::select(yr,qtr, vfci,vfci_pce) %>%
+  dplyr::select(yr,qtr, vfci, vfci_pce) %>%
   tsibble::as_tsibble() %>% 
   tsibble::filter_index(date_begin ~ date_end) %>% 
   dplyr::mutate(date=as.Date(qtr))
 
-p <- ggplot(as.data.frame(variables_fig9), aes(qtr)) +
+p <- ggplot(as.data.frame(variables_fig), aes(qtr)) +
   geom_line(aes(y=vfci,colour="VFCI"),na.rm=FALSE) +
   geom_line(aes(y=vfci_pce,colour="PCE-VFCI"),na.rm=FALSE,linetype="longdash") +
   tsibble::scale_x_yearquarter(
     name = "",
     date_labels="%Y-q%q",
-    breaks = variables_fig9$qtr[seq(1, length(variables_fig9$qtr), 40)]
+    breaks = variables_fig$qtr[seq(1, length(variables_fig$qtr), 40)]
   )  +
   theme_classic() +
   theme(
@@ -160,26 +155,25 @@ p <- ggplot(as.data.frame(variables_fig9), aes(qtr)) +
 p %>% print
 
 fname <- here::here(paste0(output_dir,"vfci_gdp_and_pce.png"))
-ggsave(fname)
+ggsave(fname, width = fig_width, height = fig_height)
 
-## Figure 10. PCA and Indiv VFCI ------------------------------------------
-base::load(file = here::here("variables.RData"))
+## PCA and Indiv VFCI ------------------------------------------
 date_begin <- "1960 Q1"
 date_end <- "2022 Q3"
 
-variables_fig10 <- variables %>%
-  dplyr::inner_join(vfci_ind,by="qtr") %>%
-  dplyr::select(yr,qtr, vfci,vfci_ind) %>%
+variables_fig <- variables %>%
+  dplyr::inner_join(results$vfci_ind, by="qtr") %>%
+  dplyr::select(yr, qtr, vfci, vfci_ind) %>%
   tsibble::as_tsibble() %>% 
   tsibble::filter_index(date_begin ~ date_end)
 
-p <- ggplot(as.data.frame(variables_fig10), aes(qtr)) +
+p <- ggplot(as.data.frame(variables_fig), aes(qtr)) +
   geom_line(aes(y=vfci,colour="VFCI"),na.rm=FALSE) +
   geom_line(aes(y=vfci_ind,colour="Individual-VFCI"),na.rm=FALSE,linetype="longdash") +
   tsibble::scale_x_yearquarter(
     name = "",
     date_labels="%Y-q%q",
-    breaks = variables_fig10$qtr[seq(1, length(variables_fig10$qtr), 40)]
+    breaks = variables_fig$qtr[seq(1, length(variables_fig$qtr), 40)]
   ) +
   theme_classic() +
   theme(
@@ -197,25 +191,25 @@ p <- ggplot(as.data.frame(variables_fig10), aes(qtr)) +
 p %>% print
 
 fname <- here::here(paste0(output_dir,"vfci_gdp_and_indiv.png"))
-ggsave(fname)
+ggsave(fname, width = fig_width, height = fig_height)
 
-## Figure 11. US and EA VFCI ----------------------------------------------
-base::load(file = here::here("variables.RData"))
+## US and EA VFCI ----------------------------------------------
 date_begin <- "1960 Q1"
 date_end <- "2022 Q3"
 
-variables_fig11 <- variables %>%
-  dplyr::select(yr,qtr, vfci,nfci) %>%
+variables_fig  <- variables %>%
+  dplyr::left_join(results$vfci_ea, by="qtr") %>%
+  dplyr::select(yr, qtr, vfci, vfci_ea) %>%
   tsibble::as_tsibble() %>% 
-  tsibble::filter_index(date_begin ~ date_end) 
+  tsibble::filter_index(date_begin ~ date_end)
 
-p <- ggplot(as.data.frame(variables_fig11), aes(qtr)) +
+p <- ggplot(as.data.frame(variables_fig), aes(qtr)) +
   geom_line(aes(y=vfci,colour="VFCI"),na.rm=FALSE) +
-  geom_line(aes(y=nfci,colour="EA-VFCI"),na.rm=FALSE,linetype="longdash") + 
+  geom_line(aes(y=vfci_ea,colour="EA-VFCI"),na.rm=FALSE,linetype="longdash") + 
   tsibble::scale_x_yearquarter(
     name = "",
     date_labels="%Y-q%q",
-    breaks = variables_fig11$qtr[seq(1, length(variables_fig11$qtr), 40)]
+    breaks = variables_fig$qtr[seq(1, length(variables_fig$qtr), 40)]
   )  +
   theme_classic() +
   theme(
@@ -233,4 +227,4 @@ p <- ggplot(as.data.frame(variables_fig11), aes(qtr)) +
 p %>% print
 
 fname <- here::here(paste0(output_dir,"us_ea_vfci.png"))
-ggsave(fname)
+ggsave(fname, width = fig_width, height = fig_height)
