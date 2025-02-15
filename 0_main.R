6b6bf30f5be7f4a905c869e3725937b88f8219a5
matdehaven <matthew_dehaven@brown.edu>
2025-02-15 08:24:46 -0500

    add internal vfci scripts to main script

--- 0_main.R
+++ 0_main.R
@@ -44,6 +44,13 @@ for (i in ff_y) {
   source('3_panel_all_models.R')
 }
 
+### Internal VFCI --------------------------------------------------------------
+source('4_internal_vfci_var_calibration.R')
+source('4_internal_vfci_var_estimation.R')
+source('4_internal_vfci_var_output.R')
+
+source('4b_internal_vfci_var_figures.R')
+
 # Robustness of the five identification schemes --------------------------------
 
 #Robustness
